b60c67b9f46b4cf825e54fec2e3619ec9a0c0d29
cainscat <vulam3102@gmail.com>
2025-01-23 06:57:09 +0700

    Login system

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,27 +47,26 @@
   <body class="login-page bg-body-secondary">
     <div class="login-box">
       <div class="card card-outline card-primary">
-        <div class="card-header">
-          <a
-            href=""
-            class="link-dark text-center link-offset-2 link-opacity-100 link-opacity-50-hover"
-          >
+        {{-- <div class="card-header">
+          <a href="" class="link-dark text-center link-offset-2 link-opacity-100 link-opacity-50-hover">
             <h1 class="mb-0"><b>Admin</b>LTE</h1>
           </a>
-        </div>
+        </div> --}}
         <div class="card-body login-card-body">
-          <p class="login-box-msg">Sign in to start your session</p>
-          <form action="" method="post">
+            @include('_message')
+          <p class="login-box-msg">Sign In</p>
+          <form action="{{ url('login') }}" method="post">
+            {{ csrf_field() }}
             <div class="input-group mb-1">
               <div class="form-floating">
-                <input id="loginEmail" type="email" class="form-control" value="" placeholder="" />
+                <input type="email" name="email" required class="form-control" value="" placeholder="" />
                 <label for="loginEmail">Email</label>
               </div>
               <div class="input-group-text"><span class="bi bi-envelope"></span></div>
             </div>
             <div class="input-group mb-1">
               <div class="form-floating">
-                <input id="loginPassword" type="password" class="form-control" placeholder="" />
+                <input name="password" type="password" required class="form-control" placeholder="" />
                 <label for="loginPassword">Password</label>
               </div>
               <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
@@ -76,8 +75,8 @@
             <div class="row">
               <div class="col-8 d-inline-flex align-items-center">
                 <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                  <label class="form-check-label" for="flexCheckDefault"> Remember Me </label>
+                  <input class="form-check-input" name="remember" type="checkbox" value="" id="remember" />
+                  <label class="form-check-label" for="remember"> Remember Me </label>
                 </div>
               </div>
               <!-- /.col -->
@@ -90,20 +89,9 @@
             </div>
             <!--end::Row-->
           </form>
-          <div class="social-auth-links text-center mb-3 d-grid gap-2">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-primary">
-              <i class="bi bi-facebook me-2"></i> Sign in using Facebook
-            </a>
-            <a href="#" class="btn btn-danger">
-              <i class="bi bi-google me-2"></i> Sign in using Google+
-            </a>
-          </div>
+
           <!-- /.social-auth-links -->
-          <p class="mb-1"><a href="forgot-password.html">I forgot my password</a></p>
-          <p class="mb-0">
-            <a href="register.html" class="text-center"> Register a new membership </a>
-          </p>
+          <p class="mb-1"><a href="{{ url('forgot-password') }}">I forgot my password</a></p>
         </div>
         <!-- /.login-card-body -->
       </div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'AuthLogin']);
+Route::get('logout', [AuthController::class, 'logout']);
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
