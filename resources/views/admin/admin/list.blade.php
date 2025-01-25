e8abca706f34641d062a283791d65e7a6a6dcd9f
cainscat <vulam3102@gmail.com>
2025-01-26 04:14:52 +0700

    pagination and form filter admin list

--- resources/views/admin/admin/list.blade.php
+++ resources/views/admin/admin/list.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-6">
-                    <h3 class="mb-0">Admin List</h3>
+                    <h3 class="mb-0">Admin List (Total: {{ $getRecord->total() }})</h3>
                 </div>
                 <div class="col-sm-6" style="text-align: right;">
                     <a href="{{ url('admin/admin/add') }}" class="btn btn-primary">Add New Admin</a>
@@ -20,9 +20,41 @@
             <div class="row">
                 <div class="col-md-12">
 
+                    <div class="card card-primary">
+                        <form action="" method="get">
+                            <div class="card-header">
+                                <h3 class="card-title">Search</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="form-group col-md-3">
+                                        <label>Name</label>
+                                        <input type="text" class="form-control" value="{{ Request::get('name') }}" name="name" placeholder="Enter Name">
+                                    </div>
+
+                                    <div class="form-group col-md-3">
+                                        <label>Email</label>
+                                        <input type="text" class="form-control" value="{{ Request::get('email') }}" name="email" placeholder="Enter Email">
+                                    </div>
+
+                                    <div class="form-group col-md-2">
+                                        <label>Date</label>
+                                        <input type="date" class="form-control" value="{{ Request::get('date') }}" name="date">
+                                    </div>
+
+                                    <div class="form-group col-md-3">
+                                        <button style="margin-top: 23px;" type="submit" class="btn btn-primary">Search</button>
+                                        <a href="{{ url('admin/admin/list') }}" style="margin-top: 23px;" class="btn btn-success">Reset</a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
                     @include('layouts._message')
 
-                    <div class="card mb-4">
+                    <div class="card mb-4 mt-3">
                         <div class="card-header">
                             <h3 class="card-title">Admin List</h3>
                         </div>
@@ -43,7 +75,7 @@
                                             <td>{{ $value->id }}</td>
                                             <td>{{ $value->name }}</td>
                                             <td>{{ $value->email }}</td>
-                                            <td>{{ $value->created_at }}</td>
+                                            <td>{{ date('d-m-Y H:i A',strtotime($value->created_at)) }}</td>
                                             <td>
                                                 <a href="{{ url('admin/admin/edit/'.$value->id) }}" class="btn btn-primary"><i class="bi bi-pencil-square"></i></a>
                                                 <a href="{{ url('admin/admin/delete/'.$value->id) }}" class="btn btn-danger"><i class="bi bi-trash3-fill"></i></a>
@@ -52,12 +84,15 @@
                                     @endforeach
                                 </tbody>
                             </table>
-                        </div> <!-- /.card-body -->
-                    </div> <!-- /.card -->
-                </div> <!-- /.col -->
-            </div> <!--end::Row-->
-        </div> <!--end::Container-->
-    </div> <!--end::App Content-->
+                            <div style="margin-top: 5px; float:right;">
+                                {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 @endsection
 
